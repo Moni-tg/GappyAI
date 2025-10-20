ebf612370a48df335fbe28c3fe92c07e6549ff36
Moni-tg <bandarachamidu97@gmail.com>
2025-10-20 14:43:39 +0530

    fdfdsf

--- app/(tabs)/Dashboard.tsx
+++ app/(tabs)/Dashboard.tsx
@@ -208,7 +208,7 @@ export default function Dashboard() {
         <HeroHeader
           title="Aquarium Dashboard"
           subtitle="Overview and health snapshot"
-          imageSource={require('D:/test/fih/GappyAI/app/components/tank.jpg')}
+          imageSource={require('app/components/tank.jpg')}
         />
 
         <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.smallCards}>
